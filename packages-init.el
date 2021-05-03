ddbaa3c8b2855871139e39da118ba57412a74712
agent18 <thetj09@gmail.com>
2021-05-03 20:55:15 +0200

    Ubuntu 20 emacs 27.1 setup removed failing marmalade-repo and removed package-initiaize at start as it was useless

--- packages-init.el
+++ packages-init.el
@@ -4,7 +4,7 @@
 (require 'package)
 
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+;(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
 
